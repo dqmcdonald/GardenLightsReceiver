3b057502babb5fa5fb287aa5aba58d7737569bbd
Quentin McDonald <dqmcdonald@gmail.com>
2020-06-12 13:20:49 +1200

    Update GardenLightsReceiver.ino
    Added Sleep button code and Real time Clock in order to have some powersavings.

--- GardenLightsReceiver.ino
+++ GardenLightsReceiver.ino
@@ -8,13 +8,16 @@
 
   Connections:
 
-    Pin  3 -> Channel One to MOSFET Gate
-    Pin  5 -> Channel Two to MOSFET Gate
+    Pin  3 -> Channel One to MOSFET Gate (PWM)
+    Pin  4 -> Connected to "sleep" switch
+    Pin  5 -> Channel Two to MOSFET Gate (PWM)
     Pin  8 -> NRF24L01 CE
     Pin 10 -> NRF24L01 CSN (Chip select in)
     Pin 11 -> NRF24L01 SDI (SPI MOSI)
     Pin 12 -> NRF24L01 SDO (SPI MOSI)
     Pin 13 -> NRF24L01 SCK (SPI Clock)
+    Pin A4 -> SDA of RTC
+    Pin A5 -> SCL of RTC
 
 
   Commands are expected as 15 byte strings. There are three sections:
@@ -31,17 +34,22 @@
 
 
 */
-
+#include "LowPower.h"
 #include <RHReliableDatagram.h>
 #include <RH_NRF24.h>
 #include <SPI.h>
+#include "RTClib.h"
 
 #define TRANSMITTER_ADDRESS 1
-#define RECEIVER_ADDRESS 2
+#define RECEIVER_ADDRESS    2
 
 
 #define CHANNEL_ONE_PIN 3
 #define CHANNEL_TWO_PIN 5
+#define SLEEP_BUTTON_PIN 4
+
+#define NUM_SLEEPS 3 // 3 sleeps of 8 seconds
+
 
 // Modes that the channels can be in:
 const int MODE_OFF = 0;
@@ -72,6 +80,7 @@ RH_NRF24 driver;
 // Class to manage message delivery and receipt, using the driver declared above
 RHReliableDatagram manager(driver, RECEIVER_ADDRESS);
 
+RTC_DS1307 rtc;
 
 void setup()
 {
@@ -92,6 +101,39 @@ void setup()
     while (true)
       delay(1000);
   }
+
+  if (! rtc.begin()) {
+    Serial.println("Couldn't find RTC");
+    while (1);
+  }
+
+  if (! rtc.isrunning()) {
+    Serial.println("RTC is NOT running, let's set the time!");
+    // When time needs to be set on a new device, or after a power loss, the
+    // following line sets the RTC to the date & time this sketch was compiled
+    rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+    // This line sets the RTC with an explicit date & time, for example to set
+    // January 21, 2014 at 3am you would call:
+    //rtc.adjust(DateTime(2020, 6, 12, 11, 28, 0));
+  } 
+  
+    Serial.println("RTC is running!");
+    DateTime now = rtc.now();
+    Serial.print(now.year(), DEC);
+    Serial.print('/');
+    Serial.print(now.month(), DEC);
+    Serial.print('/');
+    Serial.print(now.day(), DEC);
+    Serial.print("  ");
+    Serial.print(now.hour(), DEC);
+    Serial.print(':');
+    Serial.print(now.minute(), DEC);
+    Serial.print(':');
+    Serial.print(now.second(), DEC);
+    Serial.println();
+
+
+
 
   // Test each channel for a second:
   pinMode( CHANNEL_ONE_PIN, OUTPUT );
@@ -114,6 +156,8 @@ void setup()
   channel_pins[0] = CHANNEL_ONE_PIN;
   channel_pins[1] = CHANNEL_TWO_PIN;
 
+  pinMode( SLEEP_BUTTON_PIN, INPUT_PULLUP );
+
   Serial.println("Garden Lights Receiver setup Ccmplete" );
 }
 
@@ -121,46 +165,64 @@ void setup()
 // Dont put this on the stack:
 uint8_t buf[RH_NRF24_MAX_MESSAGE_LEN];
 
+
 void loop()
 {
 
-  // Check for a message from the transmitter
-  if (manager.available())
-  {
-    // Wait for a message addressed to us from the transmitter
-    uint8_t len = sizeof(buf);
-    uint8_t from;
-    if (manager.recvfromAck(buf, &len, &from))
-    {
-      Serial.print("got request from : 0x");
-      Serial.print(from, HEX);
-      Serial.print(": ");
-      Serial.println((char*)buf);
-      // Dispatch the message:
-      processCommand( (const char*)buf );
-    }
-  }
+ DateTime now = rtc.now();
 
-  // for each channel if we are in pulse mode then update the value
-  // if necessary:
-  for( int c = 0; c < NUM_CHANNELS; c++ ) {
-    if( channel_mode[c] == MODE_PULSE) {
-      if( (millis() - channel_time[c]) > channel_pulse[c] ) {
+  // If the sleep button is "on" then at any hour before 5pm or after midnight
+  // we will go to sleep. This is simply to save a bit of power. 
+  if ( digitalRead( SLEEP_BUTTON_PIN) == LOW && now.hour() < 17  ) {
+    digitalWrite( CHANNEL_ONE_PIN, LOW );
+    digitalWrite( CHANNEL_TWO_PIN, LOW );
+    // Can only sleep for 8 seconds but do it enough time to do it
+    // for a total of 24 seconds 
+    for ( int i = 0; i < NUM_SLEEPS; i++ ) {
+      LowPower.powerDown(SLEEP_8S, ADC_OFF, BOD_OFF);
+    }
+
+    Serial.println("Waking up");
+    Serial.flush();
+
+  } else {
+    // Check for a message from the transmitter
+    if (manager.available())
+    {
+      // Wait for a message addressed to us from the transmitter
+      uint8_t len = sizeof(buf);
+      uint8_t from;
+      if (manager.recvfromAck(buf, &len, &from))
+      {
+        Serial.print("got request from : 0x");
+        Serial.print(from, HEX);
+        Serial.print(": ");
+        Serial.println((char*)buf);
+        // Dispatch the message:
+        processCommand( (const char*)buf );
+      }
+    }
+
+    // for each channel if we are in pulse mode then update the value
+    // if necessary:
+    for ( int c = 0; c < NUM_CHANNELS; c++ ) {
+      if ( channel_mode[c] == MODE_PULSE) {
+        if ( (millis() - channel_time[c]) > (long unsigned )channel_pulse[c] ) {
           channel_pulse_value[c] += channel_pulse_inc[c];
-          if( channel_pulse_value[c] > 255 ) {
-              channel_pulse_value[c] = 255;
-              channel_pulse_inc[c] = -1;
+          if ( channel_pulse_value[c] > 255 ) {
+            channel_pulse_value[c] = 255;
+            channel_pulse_inc[c] = -1;
           }
-          if( channel_pulse_value[c] < 0 ) {
+          if ( channel_pulse_value[c] < 0 ) {
             channel_pulse_value[c] = 0;
-            channel_pulse_inc[c] = 1; 
+            channel_pulse_inc[c] = 1;
           }
-        analogWrite(channel_pins[c], channel_pulse_value[c]);
-        channel_time[c] = millis();
+          analogWrite(channel_pins[c], channel_pulse_value[c]);
+          channel_time[c] = millis();
+        }
       }
     }
   }
-
 
 
 }
@@ -219,7 +281,7 @@ void processCommand( const char* buf )
       analogWrite( channel_pins[channel], channel_pwm[channel] );
     }
     if ( channel_mode[channel] == MODE_PULSE ) {
-      channel_pulse[channel] = value/4;
+      channel_pulse[channel] = value / 4;
     }
 
   }
